8083012e9d631701e8111ce4206f4cc5040364d7
jackmanp <philipejackman@outlook.com>
2023-05-15 15:20:40 +0100

    Update streamlit_app.py
    dyerdj

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,14 +38,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response.json())
-
-# take the json version of the response and normalize it.
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
 #don't run anything past here whilst we troubleshoot
 streamlit.stop()
 
